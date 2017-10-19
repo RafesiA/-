8d6c12f7f2b0eaa61d9ae58e13dc796a1621056f
RafesiA <whrudwls98@naver.com>
2017-10-19 16:32:26 +0900

    Initial commit

---
+++ src/Lunch.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+class Menu{
+	File lunchDB = new File("")
+	String eatery;
+	int telNumber;
+	List<File> lucnhMenus = Arrays.asList(lunchDB);
+}
+
+public class Lunch{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+	}
+}
