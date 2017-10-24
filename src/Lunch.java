68c09255752588b8ea1bdb30504fbf09c4176216
RafesiA <whrudwls98@naver.com>
2017-10-24 11:26:14 +0900

    Basic
    Basiccc

--- src/Lunch.java
+++ src/Lunch.java
@@ -4,15 +4,55 @@ import java.util.Arrays;
 import java.util.List;
 
 class Menu{
-	File lunchDB = new File("")
+	File lunchDB = new File("C:\\Temp\\lunchDB.txt");
 	String eatery;
 	int telNumber;
 	List<File> lucnhMenus = Arrays.asList(lunchDB);
+	
+	
+	void ViewDB(){
+		FileReader fr = null;
+		InputStreamReader isr = null;
+		int i = 0;
+		
+		try {
+			
+			isr = new InputStreamReader(System.in);
+			
+			fr = new FileReader(lunchDB);
+			
+			while((i = fr.read()) != -1)
+				System.out.print((char)i);
+			
+		} catch(IOException e) {
+			System.out.println("Critical Error" + e.getMessage());
+		}
+	}
 }
 
-public class Lunch{
+
+public class Lunch {
 	public static void main(String[] args) {
+		while(true) {
+		Menu menu = new Menu();
+		System.out.println("========================");
+		System.out.println("Lunch Selection");
+		System.out.println();
+		System.out.println("골라라");
+		System.out.println("1. Show DB");
+		System.out.print("선택하기: ");
+		
 		Scanner scanner = new Scanner(System.in);
 		int choice = scanner.nextInt();
+		System.out.println("========================");
+		switch(choice){
+		
+		case 1:
+			menu.ViewDB();
+			continue;
+			
+		case 2:
+			}
+		}
 	}
 }
