c81115df3bfa3c883c27ecc486f6d0fae31b43da
RafesiA <whrudwls98@naver.com>
2017-12-15 01:20:54 +0900

    testing focusevent

--- src/Lunch.java
+++ src/Lunch.java
@@ -10,6 +10,7 @@ public class Lunch extends JFrame{
 	String filePath = resListFile.getPath();
 	JFrame frm = new JFrame();
 	JTabbedPane jtp = new JTabbedPane();
+	
 	JTextArea outputArea1, outputArea2, outputArea3, outputArea4;
 	JPanel panel1, panel2, panel3, panel4;
 	JLabel label1, label2, label3, label4;
@@ -18,7 +19,16 @@ public class Lunch extends JFrame{
 	String[] resName2 = {"선택하세요", "족발뱅이", "사또족발"};
 	String[] resName3 = {"선택하세요", "만다린", "라이징강", "짬뽕이조아", "궁차이나"};
 	
-	class MyActionListener implements ActionListener{
+	
+	
+	class MyActionListener implements ActionListener, FocusListener{
+		public void focusGained(FocusEvent f) {
+			System.out.println("Focusing..");
+		}
+		
+		public void focusLost(FocusEvent f) {
+			System.out.println("Focus Lost");
+		}
 		public void actionPerformed(ActionEvent e) {
 			JComboBox cb = (JComboBox)e.getSource();
 			String menuName = (String)cb.getSelectedItem();
@@ -141,9 +151,39 @@ public class Lunch extends JFrame{
 					System.out.println(er);
 				}
 			}
-			
-			
+			else if(menuName.equals("선택하세요")) {
+				if(resName[0] == menuName) {
+					outputArea1.setText("");
+				}
+				else if(resName1[0] == menuName) {
+					outputArea2.setText("");
+				}
+				else if(resName2[0] == menuName) {
+					outputArea3.setText("");
+				}
+				else if(resName3[0] == menuName) {
+					outputArea4.setText("");
+				}
+			}
 		}
+	}
+	
+	public void creatingMenu() {
+		JMenu firstMenu = new JMenu("파일");
+		JMenuBar menuBar = new JMenuBar();
+		String[] menuItemName = {"추가", "닫기", "계산기", "종료"};
+		JMenuItem[] menuItem = new JMenuItem[5];
+		MyActionListener listener = new MyActionListener();
+		
+		for(int i=0;i<menuItemName.length; i++) {
+			menuItem[i] = new JMenuItem(menuItemName[i]);
+			menuItem[i].addActionListener(listener);
+			firstMenu.add(menuItem[i]);
+		}
+		
+		menuBar.add(firstMenu);
+		setJMenuBar(menuBar);
+		
 	}
 	
 	void userUI() {
@@ -165,31 +205,39 @@ public class Lunch extends JFrame{
 		box2.addActionListener(listener);
 		box3.addActionListener(listener);
 		box4.addActionListener(listener);
+		box1.addFocusListener(listener);
+		box2.addFocusListener(listener);
+		box3.addFocusListener(listener);
+		box4.addFocusListener(listener);
 		
 		panel1 = new JPanel();
 		panel1.setBackground(Color.gray);
 		panel1.add(box1);
 		panel1.add(outputArea1);
 		panel1.add(new JScrollPane(outputArea1));
+		panel1.setFocusable(true);
 		
 		panel2 = new JPanel();
 		panel2.setBackground(Color.gray);
 		panel2.add(box2);
 		panel2.add(outputArea2);
 		panel2.add(new JScrollPane(outputArea2));
+		panel2.setFocusable(true);
 		
 		panel3 = new JPanel();
 		panel3.setBackground(Color.gray);
 		panel3.add(box3);
 		panel3.add(outputArea3);
 		panel3.add(new JScrollPane(outputArea3));
-		
+		panel3.setFocusable(true);
 		
 		panel4 = new JPanel();
 		panel4.setBackground(Color.gray);
 		panel4.add(box4);
 		panel4.add(outputArea4);
 		panel4.add(new JScrollPane(outputArea4));
+		panel4.setFocusable(true);
+		
 		
 		label1 = new JLabel("I'm the First");
 		label2 = new JLabel("I'm Second");
@@ -204,6 +252,8 @@ public class Lunch extends JFrame{
 		jtp.add("피자", panel2);
 		jtp.add("족발/보쌈", panel3);
 		jtp.add("중식", panel4);
+		jtp.add("+", null);
+		
 	}
 	
 	public Lunch() {
@@ -213,16 +263,11 @@ public class Lunch extends JFrame{
 		frm.setSize(1000,500);
 		frm.setVisible(true);
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
+		frm.setLayout(new BorderLayout());
 		}
 	
-	private void createMenu() {
-		JMenuBar jb = new JMenuBar();
-		JMenu file = new JMenu("File");
-		JMenuItem[] itemTitle = new JMenuItem[3];
-		String[] itemName = {"Select", "About", "Close"};
-	}
+	
+	
 	
 
 
